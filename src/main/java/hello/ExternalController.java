95d67e910c5c99834c7762961c575264e013b606
Kirill Sadovnikov <d4rkm3z@gmail.com>
2019-07-29 18:28:52 +0300

    Initial commit

---
+++ src/main/java/hello/ExternalController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/external")
+public class ExternalController {
+    private static final Logger log = LoggerFactory.getLogger(ExternalController.class);
+
+    @RequestMapping(method= RequestMethod.GET)
+    public Quote get(){
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+
+        return quote;
+    }
+}
